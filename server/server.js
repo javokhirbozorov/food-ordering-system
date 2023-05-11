11139f6cf98d53dcec210457d18978728fa3ad7f
Javokhir Bozorov <javokhir.bozorov@gmail.com>
2023-05-11 02:28:45 +0100

    upd

--- server/server.js
+++ server/server.js
@@ -8,12 +8,15 @@ import menuRoute from "./routers/menuRoute.js";
 import userRoute from './routers/userRoute.js'
 import orderRouter from "./routers/ordersRoute.js";
 dotenv.config();
+
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin: 'https://dancing-narwhal-1b2eb4.netlify.app/',
+    origin: ['https://javokhir-foodye.netlify.app', 'https://dancing-narwhal-1b2eb4.netlify.app'],
+    optionsSuccessStatus: 200,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
   }
